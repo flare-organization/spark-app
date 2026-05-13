b9dc4971b1054809a6acfa0997d721baf1b1bd9a
lembak03@hotmail.com <lembak03@hotmail.com>
2026-05-13 10:33:12 +0200

    feat: setup

---
+++ backend/src/main/java/com/bitacademy/packagemanager/backend/PingController.java
@@ -0,0 +1,17 @@
+package com.bitacademy.packagemanager.backend;
+
+import com.bitacademy.packagemanager.generated.api.model.PingResultDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/ping")
+public class PingController {
+
+    @GetMapping
+    public PingResultDto getPingResult() {
+        return new PingResultDto(true);
+    }
+
+}
