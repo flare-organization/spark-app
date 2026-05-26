bcab0a7ce8811b1bb79596abb9edd9df3d51a2a8
lembak03@hotmail.com <lembak03@hotmail.com>
2026-05-26 13:20:24 +0200

    feat: view and create for bundles

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
 import ComponentsPage from './pages/ComponentsPage'
+import BundlesPage from './pages/BundlesPage'
+import BundlesCreatePage from './pages/BundlesCreatePage'
 
 export const router = createBrowserRouter([
   {
@@ -15,5 +17,13 @@ export const router = createBrowserRouter([
   {
     path: '/components',
     element: <ComponentsPage />,
+  },
+  {
+    path: '/bundles',
+    element: <BundlesPage />,
+  },
+  {
+    path: '/bundles/create',
+    element: <BundlesCreatePage />,
   },
 ])
