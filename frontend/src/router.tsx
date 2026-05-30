718234c80a23eb586f82c7e71eb2406e5c4d0715
Richard <rsdevries07@gmail.com>
2026-05-30 10:24:06 +0200

    feat: create login page

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,11 +2,16 @@ import { createBrowserRouter } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
 import ComponentsPage from './pages/ComponentsPage'
+import LoginPage from "@/pages/LoginPage.tsx";
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+  },
+    {
+    path: '/login',
+    element: <LoginPage />,
   },
   {
     path: '/about',
