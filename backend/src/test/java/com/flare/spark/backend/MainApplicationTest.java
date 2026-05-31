254e0069de6bf517883613f554ae6884fe274af4
Richard <rsdevries07@gmail.com>
2026-05-31 10:47:48 +0200

    feat: add abstract integrationtest and fix jakarta validation error
    - add spring-boot-starter-validation dependency to fix jakarta validation error
    - removed jakarta.validation dependency
    - add abstract integration class for integration testing

--- backend/src/test/java/com/flare/spark/backend/MainApplicationTest.java
+++ backend/src/test/java/com/flare/spark/backend/MainApplicationTest.java
@@ -3,27 +3,8 @@ package com.flare.spark.backend;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
-@SpringBootTest
-class MainApplicationTest {
-
-    @Container
-    static PostgreSQLContainer<?> postgres =
-            new PostgreSQLContainer<>("postgres:18-alpine");
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-    }
+class MainApplicationTest extends IntegrationTest {
 
     @BeforeAll
     static void beforeAll() {
@@ -36,6 +17,6 @@ class MainApplicationTest {
     }
 
     @Test
-    void contextLoads() {
+    void appRuns() {
     }
 }
