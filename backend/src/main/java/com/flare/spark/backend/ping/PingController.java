17185b425bbfa46f059704753e9c8c88912a34f8
Richard de Vries <rsdevries07@gmail.com>
2026-06-03 11:31:34 +0200

    remove requestmappings on ping controller

--- backend/src/main/java/com/flare/spark/backend/ping/PingController.java
+++ backend/src/main/java/com/flare/spark/backend/ping/PingController.java
@@ -1,23 +1,13 @@
 package com.flare.spark.backend.ping;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class PingController {
 
-    @RequestMapping(
-        value = "/api/v1/ping",
-        method = {
-            RequestMethod.GET,
-            RequestMethod.POST,
-            RequestMethod.PUT,
-            RequestMethod.PATCH,
-            RequestMethod.DELETE,
-        }
-    )
+    @GetMapping("/api/v1/ping")
     public ResponseEntity<Void> ping() {
         return ResponseEntity.noContent().build();
     }
