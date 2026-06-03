75f5ab0f3bec74ecf27300f52ffb65518b3fa649
Richard de Vries <rsdevries07@gmail.com>
2026-06-03 15:24:31 +0200

    rename createBundleRequest to createBundle

--- frontend/src/services/bundleService.ts
+++ frontend/src/services/bundleService.ts
@@ -1,6 +1,6 @@
 import {http} from "@/lib/http/http.ts";
-import type { Bundle } from "@openapi/model/bundle.ts";
-import type {CreateBundleRequest} from "@openapi/model/createBundleRequest.ts";
+import type {Bundle} from "@openapi/model/bundle.ts";
+import type {CreateBundle} from "@openapi/model/createBundle.ts";
 
 export async function getBundles(): Promise<Bundle[]> {
     const res = await http.get<Bundle[]>('/api/v1/bundles')
@@ -8,7 +8,7 @@ export async function getBundles(): Promise<Bundle[]> {
     return res.data
 }
 
-export async function createBundle(bundleRequest: CreateBundleRequest): Promise<Bundle> {
+export async function createBundle(bundleRequest: CreateBundle): Promise<Bundle> {
     const res = await http.post<Bundle>('/api/v1/bundles', bundleRequest);
 
     return res.data
