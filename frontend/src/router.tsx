30b8ed379c7487b8313f741126bd7b12e1f2e3e3
Richard de Vries <144018443+Richard-2oo7@users.noreply.github.com>
2026-06-05 22:03:17 +0200

    Revert "T17 user can create and view a bundle"

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,8 +2,6 @@ import { createBrowserRouter } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
 import ComponentsPage from './pages/ComponentsPage'
-import BundlesPage from './pages/BundlesPage'
-import BundlesCreatePage from './pages/BundlesCreatePage'
 
 export const router = createBrowserRouter([
   {
@@ -17,13 +15,5 @@ export const router = createBrowserRouter([
   {
     path: '/components',
     element: <ComponentsPage />,
-  },
-  {
-    path: '/bundles',
-    element: <BundlesPage />,
-  },
-  {
-    path: '/bundles/create',
-    element: <BundlesCreatePage />,
   },
 ])
